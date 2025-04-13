65af5d43364a229fd8f93909275ebcd37dc282a6
Bob Zhang <bobzhang55@yahoo.com>
2025-04-13 11:35:29 -0500

    ch03 - copy book code into my/src folder of "my" project for runing and tests.

---
+++ book-code-listings/chapter03/my/src/BookCode/Welcome.js
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+export function Welcome() {
+  let header = <Header />;
+  return (
+    <>
+      <div>{header}</div>
+      
+    </>
+  );
+}
+// export default Welcome;
