6ffab067c6cb4b6155d830c36a528502916c15a4
Bob Zhang <bobzhang55@yahoo.com>
2025-04-17 12:18:38 -0500

    Ch03-Run MyfancyComponent

--- book-code-listings/chapter03/my/src/App.js
+++ book-code-listings/chapter03/my/src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 // import { Login as App } from './BookCode/Login';
 // import { ImmediateInvoke as App } from './BookCode/ImmediateInvoke'; 
 // import { Welcome as App } from './BookCode/ConditionalWelcome';
-import { SearchBox as App } from './BookCode/SearchBox/SearchBox';  
+// import { SearchBox as App } from './BookCode/SearchBox/SearchBox';  
+import { MyFancyComponent as App } from './BookCode/MyFancyComponent/MyFancyComponent'; 
 
 
 export default App;
